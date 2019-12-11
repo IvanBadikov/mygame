7aaa91b674d9866a863b805323ad164f6b11b4ab
IvanBadikov <scp.conditional.team@gmail.com>
2019-12-12 02:34:30 +0300

    First commit
    - Walking;
    - Joystick look;
    - A room;
    - Dark.

---
+++ Assets/Scripts/Movement/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Joystick movementInput;
+    private CharacterController controller;
+
+    private Vector3 movementDirection;
+    private Vector3 forwardDirection;
+    private Vector3 sideDirection;
+
+    private void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        /*movementDirection = new Vector3(
+            movementInput.Horizontal,
+            0,
+            movementInput.Vertical
+        );*/
+
+        forwardDirection = transform.TransformDirection(Vector3.forward);
+        sideDirection = transform.TransformDirection(Vector3.right);
+
+        movementDirection = (forwardDirection * movementInput.Vertical) + (sideDirection * movementInput.Horizontal);
+
+        controller.Move(movementDirection * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Movement/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    [SerializeField]
+    private Joystick lookingInput;
+    [SerializeField]
+    private float rotateSpeed;
+
+    private CharacterController controller;
+
+    private void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+    private void Update() {
+        //transform.rotation = 
+        //    transform.rotation * Quaternion.Euler(0, rotateSpeed * lookingInput.Horizontal, 0);
+        transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime * lookingInput.Horizontal, 0), Space.Self);
+    }
+}
